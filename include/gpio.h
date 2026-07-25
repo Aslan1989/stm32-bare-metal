fbea596e3f77b60db7f82f054afe9c8c3bf1b144
AslanIsaev <bonscott.isaev@gmail.com>
2026-07-25 17:59:43 +0200

    implement pin set and clear

--- include/gpio.h
+++ include/gpio.h
@@ -2,5 +2,7 @@
 #define GPIO_H
 
 void gpio_init(void);
+void gpio_set_pin(GPIO_TypeDef *GPIOx, uint8_t pin);
+void gpio_clear_pin(GPIO_TypeDef *GPIOx, uint8_t pin);
 
 #endif // GPIO_H
--- src/gpio.c
+++ src/gpio.c
@@ -5,4 +5,17 @@ void gpio_init(void)
 {
 	// Implementation for GPIO initialization
 	RCC->APB2ENR |= (1 << 2); // Enable GPIOA clock
+
+	GPIOA->CRL &= ~(0xF << 20); // Clear CNF and MODE bits for PA5
+	GPIOA->CRL |= (0x1 << 20); // Set MODE bits for
+}
+
+void gpio_set_pin(GPIO_TypeDef *GPIOx, uint8_t pin)
+{
+	GPIOx->BSRR = (1U << pin); // Set the specified pin
+}
+
+void gpio_clear_pin(GPIO_TypeDef *GPIOx, uint8_t pin)
+{
+	GPIOx->BSRR = (1U << (pin + 16)); // Clear the specified pin
 }
