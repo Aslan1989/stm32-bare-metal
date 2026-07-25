3af1fd6f1cc9e043592813f5e1a7bb21d14a027f
AslanIsaev <bonscott.isaev@gmail.com>
2026-07-25 18:03:39 +0200

    Implement GPIO driver API

--- include/gpio.h
+++ include/gpio.h
@@ -2,7 +2,8 @@
 #define GPIO_H
 
 void gpio_init(void);
-void gpio_set_pin(GPIO_TypeDef *GPIOx, uint8_t pin);
-void gpio_clear_pin(GPIO_TypeDef *GPIOx, uint8_t pin);
+void gpio_set_pin(GPIO_TypeDef *gpio, uint8_t pin);
+void gpio_clear_pin(GPIO_TypeDef *gpio, uint8_t pin);
+void gpio_toggle_pin(GPIO_TypeDef *gpio, uint8_t pin);
 
 #endif // GPIO_H
--- src/gpio.c
+++ src/gpio.c
@@ -6,16 +6,24 @@ void gpio_init(void)
 	// Implementation for GPIO initialization
 	RCC->APB2ENR |= (1 << 2); // Enable GPIOA clock
 
+	// PA5 as output (push-pull, max speed 2 MHz)
 	GPIOA->CRL &= ~(0xF << 20); // Clear CNF and MODE bits for PA5
-	GPIOA->CRL |= (0x1 << 20); // Set MODE bits for
+	GPIOA->CRL |= (0x1 << 20); // Set MODE bits for PA5
 }
 
-void gpio_set_pin(GPIO_TypeDef *GPIOx, uint8_t pin)
+// Set the specified pin
+void gpio_set_pin(GPIO_TypeDef *gpio, uint8_t pin)
 {
-	GPIOx->BSRR = (1U << pin); // Set the specified pin
+	gpio->BSRR = (1U << pin); // Set the specified pin
 }
 
-void gpio_clear_pin(GPIO_TypeDef *GPIOx, uint8_t pin)
+// Clear the specified pin
+void gpio_clear_pin(GPIO_TypeDef *gpio, uint8_t pin)
 {
-	GPIOx->BSRR = (1U << (pin + 16)); // Clear the specified pin
+	gpio->BSRR = (1U << (pin + 16)); // Clear the specified pin
+}
+
+void gpio_toggle_pin(GPIO_TypeDef *gpio, uint8_t pin)
+{
+	gpio->ODR ^= (1U << pin); // Toggle the specified pin
 }
